1a0aa23d9d5ce76a3925e8f3e385d874d6f5f3cb
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-09 17:40:27 +0000

    Initial Commit

---
+++ DownloadImageSelfClone/DownloadImageSelfCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadImageSelfCloneApp.swift
+//  DownloadImageSelfClone
+//
+//  Created by Kyungyun Lee on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DownloadImageSelfCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
